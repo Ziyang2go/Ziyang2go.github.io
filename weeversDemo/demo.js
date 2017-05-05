951929ccf1040463a0794a674f49cce53b7b5270
Ziyang <wangziyang507@gmail.com>
2017-05-04 23:07:13 -0400

    add responsive, add clear

--- weeversDemo/demo.js
+++ weeversDemo/demo.js
@@ -1,19 +1,25 @@
 var api = claraplayer('clara-embed');
-
+var canvasId;
 api.sceneIO.fetchAndUse('4a06a7a6-1c7a-4e01-babb-654ad53051f9');
+
 api.on('loaded', function(){
   api.player.useCamera('fbb45c46-8a7c-44c8-b1e2-245e94f2e250');
   ['orbit', 'pan', 'zoom', 'home', 'fullscreen'].forEach(function(tool){
     api.player.hideTool(tool);
   });
   canvasId = api.scene.find({name:'baseMap'});
-  setInterval(function() {
-    api.sceneGraph.touch(canvasId)
-  }, 1000/15);
 });
 
 var canvas = new fabric.Canvas('sourceCanvas');
-var background
+
+canvas.on({
+  'touch:drag': function() {
+    api.sceneGraph.touch(canvasId);
+  },
+  'mouse:move': function() {
+    api.sceneGraph.touch(canvasId);
+  },
+});
 
 document.getElementById('buttonInputText').addEventListener('click', function(e) {
   var text = document.getElementById('inputText').value;
@@ -22,6 +28,7 @@ document.getElementById('buttonInputText').addEventListener('click', function(e)
     top: 100,
     fill: 'black',
   }));
+  api.sceneGraph.touch(canvasId);
 });
 
 document.getElementById('upload').addEventListener("change", function (e) {
@@ -51,3 +58,22 @@ document.getElementById('buttonDownloadPDF').addEventListener('click', function(
 
   pdf.save("download.pdf");
 });
+
+//responsive resize canvas object
+canvas.setHeight($(window).height()/1.2);
+canvas.setWidth($(window).width()/2.5);
+canvas.renderAll();
+
+$(window).resize(function(){
+  canvas.setHeight($(window).height()/1.2);
+  canvas.setWidth($(window).width()/2.5);
+  canvas.renderAll();
+})
+
+function clearCanvas() {
+  var imageName = document.getElementById('filename');
+  var textName = document.getElementById('inputText');
+  imageName.value = '';
+  textName.value = '';
+  canvas.clear();
+}
