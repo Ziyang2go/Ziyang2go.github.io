453f84726c05f2b8532ae3c6dd58b27d6ff19218
Ziyang Wang <ziyang@exocortex.com>
2017-09-05 12:51:45 -0400

    test offline player

---
+++ clara-pricing/translation.js
@@ -0,0 +1,126 @@
+export default {
+  en: {
+    //view buttons
+    Top: 'Top',
+    Left: 'Left',
+    Front: 'Front',
+    Bottom: 'Bottom',
+    Right: 'Right',
+    Back: 'Back',
+    //configuration header
+    Box: 'Box',
+    Artwork: 'Artwork',
+    //box type
+    'Standard Box': 'Standard Box',
+    'Ellipse Box': 'Ellipse Box',
+    //scaler
+    Length: 'Length',
+    Width: 'Width',
+    Depth: 'Depth',
+    //artwork
+    'Add Artwork': 'Add Artwork',
+    'info.upload.content': 'File formats: .jpg, .png',
+    'warning.upload.content': 'Artwork is too large for the box',
+    'success.upload.content': 'Artwork uploaded sucessfully',
+    //add text
+    'Add Text': 'Add Text',
+    'Times New Roman': 'Times New Roman',
+    Arial: 'Arial',
+    'Courier New': 'Courier New',
+    'text.default': 'Edit Text Here',
+    //canvas opertaion
+    'canvas.fill': 'Fill',
+    'canvas.fit': 'Fit',
+    'canvas.center': 'Center',
+    'canvas.delete': 'Delete',
+    'cavans.background': 'Background',
+    'canvas.applyall': 'Apply to All Sides',
+    //price
+    Price: 'Price',
+    Quantity: 'Quantity',
+    'Add to Cart': 'Add to Cart',
+  },
+  nl: {
+    //view buttons
+    Top: 'Top',
+    Left: 'Links',
+    Front: 'Voorkant',
+    Bottom: 'Bodem',
+    Right: 'Rechts',
+    Back: 'Terug',
+    //configuration header
+    Box: 'Doos',
+    Artwork: 'artwork',
+    //box type
+    'Standard Box': 'Standaard Doos',
+    'Ellipse Box': 'Ellips Doos',
+    //scaler
+    Length: 'Lengte',
+    Width: 'Breedte',
+    Depth: 'Diepte',
+    //artwork
+    'Add Artwork': 'Voeg kunstwerk toe',
+    'info.upload.content': 'Bestandsindelingen: .jpg, .png',
+    'warning.upload.content': 'Het kunstwerk is te groot voor de doos',
+    'success.upload.content': 'Artwork is succesvol geüpload',
+    //add text
+    'Add Text': 'Voeg tekst toe',
+    'Times New Roman': 'Times New Roman',
+    Arial: 'Arial',
+    'Courier New': 'Courier New',
+    'text.default': 'Tekst hier aanpassen',
+    //canvas opertaion
+    'canvas.fill': 'Vullen',
+    'canvas.fit': 'Fit',
+    'canvas.center': 'Centrum',
+    'canvas.delete': 'Verwijder',
+    'cavans.background': 'Achtergrond',
+    'canvas.applyall': 'Toepassen op alle zijden',
+    //price
+    Price: 'Prijs',
+    Quantity: 'Aantal stuks',
+    'Add to Cart': 'Voeg toe aan winkelmandje',
+  },
+  de: {
+    //view buttons
+    Top: 'Oben',
+    Left: 'Links',
+    Front: 'Vorderseite',
+    Bottom: 'Boden',
+    Right: 'Recht',
+    Back: 'Zurück',
+    //configuration header
+    Box: 'Box',
+    Artwork: 'Kunstwerk',
+    //box type
+    'Standard Box': 'Standard Box',
+    'Ellipse Box': 'Ellipse Box',
+    //scaler
+    Length: 'Länge',
+    Width: 'Breite',
+    Depth: 'Tiefe',
+    //artwork
+    'Add Artwork': 'Kunst Hinzufügen',
+    'info.upload.content': 'Dateiformate: .jpg, .png',
+    'warning.upload.content': 'Artwork ist zu groß für die Box',
+    'success.upload.content': 'Artwork erfolgreich hochgeladen',
+    //add text
+    'Add Text': 'Text hinzufügen',
+    'Times New Roman': 'Times New Roman',
+    Arial: 'Arial',
+    'Courier New': 'Courier New',
+    'text.default': 'Text bearbeiten hier',
+
+    //canvas opertaion
+    'canvas.fill': 'Füllen',
+    'canvas.fit': 'Passen',
+    'canvas.center': 'Center',
+    'canvas.delete': 'Löschen',
+    'cavans.background': 'Hintergrund',
+    'canvas.applyall': 'Auf alle Seiten anwenden',
+    //price
+    Price: 'Preis',
+    Quantity: 'Anzahl',
+    'Add to Cart': 'in den Warenkorb legen',
+  },
+};
