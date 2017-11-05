1f9125827441175382a6cde28701e27a61f57e75
Ziyang <wangziyang507@gmail.com>
2017-11-05 14:16:45 -0500

    add logging, no action and value passed in

--- testGA/claraplayer-2.3.42.js
+++ testGA/claraplayer-2.3.42.js
@@ -33692,6 +33692,9 @@ function addGoogleAnalytics(id, name) {
     if (!id || !name) return _promise2.default.reject(new Error('Google Analytics should have id and name'));
     (0, _player.initGA)(id, name);
     var trackFn = function trackFn(attribute, value) {
+      console.log('Changing configuration ..........');
+      console.log(attribute);
+      console.log(value);
       return store.trackEvent(name, 'Configuration Change', attribute, value);
     };
     store.dispatch(onChange(trackFn));
